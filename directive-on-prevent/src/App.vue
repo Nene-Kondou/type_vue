e67fa6cfa87c1c46b5ffd8695f25eae121043772
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-08-29 08:19:02 +0900

    event prevent

---
+++ directive-on-prevent/src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const msg = ref("未送信");
+const onFormSubmit = (): void => {
+  msg.value = "送信されました";
+}
+</script>
+
+<template>
+  <div>
+    <form action="#" v-on:submit.prevent="onFormSubmit">
+      <input type="text" name="" id="" required>
+      <button type="submit">送信</button>
+    </form>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>

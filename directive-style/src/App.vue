c97019b7c98d9382f2e6be102bbbb087af34a3e0
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-08-29 06:21:32 +0900

    v-bind:style属性へのバインディング

---
+++ directive-style/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {ref,computed} from "vue";
+
+const msg = ref("こんにちは！世界");
+const msgTextRed = ref("red");
+const msgTextColor = ref("white");
+const msgbgtColor = ref("black");
+const msgStyles = ref({
+  color: "white",
+  backgroundColor: "black"
+});
+const msgStyles2 = ref({
+  fontSize: "24pt"
+});
+const msgStyles3 = ref({
+  color: "pink",
+  fontSize: "24pt"
+});
+const textSize = computed(
+  (): string => {
+    const size = Math.round(Math.random() * 25) + 10;
+    return `${size}pt`;
+  }
+)
+</script>
+
+<template>
+  <div>
+    <p v-bind:style="{color: msgTextRed}">{{ msg }}</p>
+    <p v-bind:style="{color: 'pink'}">{{ msg }}</p>
+    <p v-bind:style="{fontSize: textSize}">{{ msg }}</p>
+    <p v-bind:style="{color: msgTextColor,backgroundColor: msgbgtColor}">{{ msg }}</p>
+    <p v-bind:style="{color: msgTextColor, 'background-color': msgbgtColor}">{{ msg }}</p>
+    <p v-bind:style="msgStyles">{{ msg }}</p>
+    <p v-bind:style="[msgStyles, msgStyles2]">{{ msg }}</p>
+    <p v-bind:style="[msgStyles, msgStyles3]">{{ msg }}</p>
+    <p v-bind:style="[msgStyles3, msgStyles]">{{ msg }}</p>
+  </div>
+</template>
+

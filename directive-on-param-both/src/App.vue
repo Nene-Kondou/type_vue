de6fa4876b30a1a546ac180ea450e4cf0c505297
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-08-29 08:12:33 +0900

    イベントオブジェクトとそのほかの引数を併用するイベントハンドラメソッド

---
+++ directive-on-param-both/src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const pMsg = ref("イベント前(ここをクリック!)");
+const pBgColorEvent = ref("white");
+const onPClickWithEvent = (bgColor: string, event: MouseEvent): void => {
+  pBgColorEvent.value = bgColor;
+  pMsg.value = event.timeStamp.toString();
+}
+</script>
+
+<template>
+  <p v-on:click="onPClickWithEvent('green', $event)" v-bind:style="{backgroundColor: pBgColorEvent}">
+    {{ pMsg }}
+  </p>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>

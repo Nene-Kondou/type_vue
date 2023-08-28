0aba24333fff09cd41c02f19174a76d29777953f
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-08-28 22:51:20 +0900

    バインドする属性をテンプレート変数として指定

---
+++ directive-bind-dynamic/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const widthOrHeight = ref("height");
+const widthOrHeightValue = ref(100);
+</script>
+
+<template>
+  <p><img src="./assets/logo.svg" v-bind:[widthOrHeight]="widthOrHeightValue" alt=""></p>
+</template>
+

c21541bd7689ff93b7214418b52202fe1fc082cd
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-08-30 07:08:40 +0900

    コンポーネントの基本

---
+++ components-basics/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// import OneSection from "./components/OneSection.vue";
+import OneSection from "./components/OneSection.vue";
+</script>
+
+<template>
+  <div>
+    <h1>コンポーネント基礎</h1>
+    <section>
+      <h2>コンポーネント1個</h2>
+      <OneSection />
+    </section>
+    <section>
+      <h2>コンポーネントが複数</h2>
+      <OneSection></OneSection>
+      <OneSection></OneSection>
+      <OneSection></OneSection>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>

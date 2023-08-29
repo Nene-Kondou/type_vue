95df2d945b7635cd63619b636eb5506280f1a51a
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-08-30 07:24:29 +0900

    コンポーネントの独立性

---
+++ components-vmodel/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import WidthModel from './components/WidthModel.vue'
+</script>
+
+<template>
+  <div>
+    <h1>コンポーネントの独立性</h1>
+    <WidthModel></WidthModel>
+    <WidthModel></WidthModel>
+  </div>
+</template>
+
+<style scoped>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>

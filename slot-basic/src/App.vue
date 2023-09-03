3b71e8e4cfbbdd19f7dccf6d1ac0d44b99d6b39c
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-09-03 16:14:18 +0900

    slot

---
+++ slot-basic/src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import OneSection from "./components/OneSection.vue";
+
+const taro = ref("田中太郎");
+</script>
+
+<template>
+  <section>
+    <h2>Slotの利用</h2>
+    <OneSection v-bind:name="taro">
+      <p>連絡がつきません。</p>
+    </OneSection>
+  </section>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
